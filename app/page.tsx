cd61903814c02bb959663a40c2b49fa20c4e4130
Lpreet <lpreet.nerd@gmail.com>
2024-01-21 15:32:15 +0100

    Mobile and tablet changes, more responsive

--- app/page.tsx
+++ app/page.tsx
@@ -8,22 +8,22 @@ import { ArrowDown } from "lucide-react";
 export default function Home() {
   return (
     <main className="bg-white">
-      <header className="flex mx-4 justify-between">
-        <Image className="mt-2" src="/logo.png" alt="logo" width={150} height={150} />
-        <div className="space-x-8 mr-2 mt-4">
+      <header className="flex md:mx-4 md:justify-between justify-center">
+        <Image className="mt-2 mr-2 md:mr-0" src="/logo.png" alt="logo" width={150} height={150} />
+        <div className="space-x-8 mr-2 mt-4 md:block hidden">
           <Button>Login</Button>
           <Button>Sign up</Button>
         </div>
       </header>
       <section className="relative">
         <div className="background-image hidden lg:block" style={{ height: "760px", width: "1152px" }} />
-        <div className="lg:absolute lg:top-16 lg:right-20 flex items-center justify-center lg:mr-12 mt-24 lg:mt-10">
+        <div className="lg:absolute lg:top-16 lg:right-20 flex items-center justify-center lg:mr-12 mt-10 lg:mt-10">
           <div className="bg-white p-4 border-4 border-double border-slate-500 rounded-lg">
             <Form />
           </div>
         </div>
       </section>
-      <div className="lg:block hidden">
+      <div className="md:block hidden">
       <h1 className="text-center text-xl font-bold mb-8">Brands that use our service</h1>
       <div className="flex">
         <div className="flex-1" />
