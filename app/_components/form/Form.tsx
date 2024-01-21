2065617233013402076da7471029cb5620b4eae8
Lpreet <lpreet.nerd@gmail.com>
2024-01-21 13:28:05 +0100

    UX/UI of the Login/Sign up form is done.

--- app/_components/form/Form.tsx
+++ app/_components/form/Form.tsx
@@ -1,10 +1,12 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
+import React, { useState } from "react";
 
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,56 +14,99 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
 
 const formSchema = z.object({
   username: z.string().min(4, {
     message: "Username must be at least 4 characters.",
   }),
-})
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
 
 export default function FormModel() {
-
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      password: "",
     },
-  })
- 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-  }
+    mode: "onSubmit",
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {}
+
+  const handleButtonClick = (button: string) => {
+    setActiveButton(button);
+    form.reset({
+      username: "",
+      password: "",
+    });
+  };
 
   const providers = [
     { name: "Facebook", icon: Facebook },
     { name: "Linkedin", icon: Linkedin },
     { name: "Instagram", icon: Instagram },
-    { name: "Twitter", icon: Twitter}
-  ]
+    { name: "Twitter", icon: Twitter },
+  ];
+
+  const [activeButton, setActiveButton] = useState<string>('login');
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}  className="space-y-4 w-[260px]">
-        <h1 className="text-center text-xl text-[#DF0000] font-bold ">Welcome</h1>
-        <div className="flex justify-evenly ">
-        <Button variant="ghost">Login</Button>
-        <Button variant="ghost">Sign up</Button>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-2 w-[272px]"
+      >
+        <h1 className="text-center text-xl text-[#DF0000] font-bold mb-4">
+          Welcome
+        </h1>
+        <div className="flex justify-evenly">
+        <Button 
+            type="button"
+            variant={activeButton === 'login' ? 'default' : 'ghost'}
+            onClick={() => handleButtonClick('login')}
+            
+          >
+            Login
+          </Button>
+          <Button 
+            type="button"
+            variant={activeButton === 'signup' ? 'default' : 'ghost'}
+            onClick={() => handleButtonClick('signup')}
+          >
+            Sign up
+          </Button>
         </div>
-        <p>Log in using any of these providers</p>
-        
+        <div className="space-y-4">
+        {activeButton === 'login' ? (
+            <p>Log in using any of these providers</p>
+          ) : (
+            <p>Sign up using any of these providers</p>
+          )}
         <div className="flex space-x-4">
           {providers.map((provider) => (
             <Button key={provider.name} type="button">
               <provider.icon className="w-5 h-5" />
             </Button>
           ))}
-          
         </div>
-        <p className="text-center"><b>Or</b></p>
-        <p className="text-center">Log in with your username</p>
-          <FormField
+        </div>
+        <div className="text-center space-y-2">
+        <p>
+          <b>Or</b>
+        </p>
+          {activeButton === 'login' ? (
+            <p>Log in with your username</p>
+          ) : (
+            <p>Sign up with your username</p>
+          )}
+          </div>
+        <FormField
           control={form.control}
           name="username"
           render={({ field }) => (
@@ -74,8 +119,21 @@ export default function FormModel() {
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="password..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,17 @@ import Form from "./_components/form/Form";
 export default function Home() {
   return (
     <main className="bg-white">
-      <header className="flex space-y-2 mx-2 justify-between">
-        <Image src="/logo.png" alt="logo" width={150} height={150} />
-        <div className="space-x-4">
-          <Button>Sign up</Button>
+      <header className="flex mx-4 justify-between">
+        <Image className="mt-2" src="/logo.png" alt="logo" width={150} height={150} />
+        <div className="space-x-8 mr-2 mt-4">
           <Button>Login</Button>
+          <Button>Sign up</Button>
         </div>
       </header>
       <section className="relative">
-        <div className="background-image hidden lg:block" style={{ height: "700px", width: "1150px" }} />
-        <div className="lg:absolute lg:top-20 lg:right-20 flex items-center justify-center lg:mr-16 mt-28 lg:mt-10">
-          <div className="bg-slate-50 p-4 border-4 border-double border-slate-500 rounded-lg">
+        <div className="background-image hidden lg:block" style={{ height: "760px", width: "1152px" }} />
+        <div className="lg:absolute lg:top-16 lg:right-20 flex items-center justify-center lg:mr-12 mt-24 lg:mt-10">
+          <div className="bg-white p-4 border-4 border-double border-slate-500 rounded-lg">
             <Form />
           </div>
         </div>
